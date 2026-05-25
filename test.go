d6dfd8832e21e960ca8d9453009c60b8271c138d
axrorback <axrorback@gmail.com>
2026-05-25 11:06:30 +0500

    Add `test.go` with a simple example program

---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func yoshimni_chiqarib_ber() {
+	yoshim := 21
+	fmt.Println("Mening yoshim", yoshim, "da")
+}
+
+func main() {
+	fmt.Println("Go ishlamoqda")
+	yoshimni_chiqarib_ber()
+}
